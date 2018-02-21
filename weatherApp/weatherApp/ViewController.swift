97265af9188b62e9d44dbb2961dd359c31601be5
polik <polikhill@gmail.com>
2018-02-21 19:54:31 +0200

    Location

--- weatherApp/weatherApp/ViewController.swift
+++ weatherApp/weatherApp/ViewController.swift
@@ -2,20 +2,51 @@
 //  ViewController.swift
 //  weatherApp
 //
-//  Created by Polina on 2/21/18.
+//  Created by Polina on 2/20/18.
 //  Copyright © 2018 Polina. All rights reserved.
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController : UIViewController  {
+    
+    var locationManager: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        print("del")
+        locationManager.startUpdatingLocation()
+        print("startupdate")
+        startLocation = nil
+        print("nil")
     }
-
-
-
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation])
+    {
+        let latestLocation: CLLocation = locations.last!
+        
+        print(String(format: "%.4f", latestLocation.coordinate.latitude))
+        print(String(format: "%.4f", latestLocation.coordinate.longitude))
+        
+        if startLocation == nil {
+            startLocation = latestLocation
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didFailWithError error: Error) {
+        print("can't get current location")
+    }
+    
+}
